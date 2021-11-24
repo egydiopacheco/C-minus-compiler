175140d5e727be6190fc18053bc982c56224cea2
Egydio Pacheco <egydio.neto@usp.br>
2021-11-24 16:52:58 -0300

    add s-exp

--- src/frontend/ast.ml
+++ src/frontend/ast.ml
@@ -1,34 +1,15 @@
 type typ =
     | TInt
     | TVoid
+[@@deriving sexp]
 
-type program = decl list
-and decl =
-    | SetVar of typ * string
-    | SetArr of typ * string * int
-    | SetFunc of typ * string * param list * var_decl list * stmt list
-
-and var_decl =
-    | VarSpecs of typ * string
-    | ArrSpecs of typ * string * int
-
-and param = 
-    | VarParams of typ * string
-    | ArrParams of typ * string
-
-and stmt =
-    | ExprDecl of expr option
-    | ScopeDecl of var_decl list * stmt list
-    | CondDecl of expr * stmt * stmt option
-    | WhileDecl of expr * stmt
-    | RetDecl of expr option
-
-and expr = 
+type expr = 
     | AssignExpr of string * expr
     | AssignArr of string * expr * expr
     | AssignMath of math_expr
-    | AssignLogic of relop_expr
-
+    | AssignLogic of relop_expr        
+[@@deriving sexp]
+        
 and math_expr =
     | Add of math_expr * math_expr
     | Sub of math_expr * math_expr
@@ -46,4 +27,32 @@ and relop_expr =
     | Gtr of math_expr * math_expr
     | Gte of math_expr * math_expr
     | Eq of math_expr * math_expr
-    | Neq of math_expr * math_expr
+    | Neq of math_expr * math_expr             
+[@@deriving sexp]
+
+type var_decl =
+    | VarSpecs of typ * string
+    | ArrSpecs of typ * string * int
+[@@deriving sexp]
+
+type param = 
+    | VarParams of typ * string
+    | ArrParams of typ * string
+[@@deriving sexp]
+
+type stmt =
+    | ExprDecl of expr option
+    | ScopeDecl of var_decl list * stmt list
+    | CondDecl of expr * stmt * stmt option
+    | WhileDecl of expr * stmt
+    | RetDecl of expr option
+[@@deriving sexp]
+
+type decl =
+    | SetVar of typ * string
+    | SetArr of typ * string * int
+    | SetFunc of typ * string * param list * var_decl list * stmt list
+[@@deriving sexp]
+
+type program = decl list
+[@@deriving sexp]
