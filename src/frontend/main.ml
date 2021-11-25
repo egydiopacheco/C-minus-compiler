79f9a602977eb66c7373e133c3e3b06ecc758f40
Egydio Pacheco <egydio.neto@usp.br>
2021-11-25 00:32:44 -0300

    new main

--- src/frontend/main.ml
+++ src/frontend/main.ml
@@ -1,19 +1,8 @@
-open Lexer
-open Hashtable
-
-(* This is just an alias of the Hashtable module *)
-module H = Hashtable
-
-(* This function open a lexbuf and add the output to a hash table *)
-let main () =
-  let my_buf = set_filename "stdin" (Lexing.from_channel stdin) in
-  let ht = H.create_hash 300 in
-  let rec loop = function
-    | EOF -> H.to_table EOF ht
-    | x   -> (H.to_table x ht); loop (token my_buf)
-  in
-  loop (token my_buf);
-  (* Uncomment this line to print the hash table *)
-  H.print_hash ht 
-  let () = main ()
-
+open Pp
+    
+let parse = 
+  print_string "AST - PrettyPrinting \n\n";
+  let prog = open_in Sys.argv.(1) in
+  let lexbuf = Lexing.from_channel prog in
+  let ast = Parser.program Lexer.token lexbuf in
+  pretty_print ast
